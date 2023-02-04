f5c47404ee13262315b27ac8b516228526dd6d7f
edward-e-daisey <121644982+edward-e-daisey@users.noreply.github.com>
2023-02-04 15:20:20 -0600

    Update main.py

--- Project 0/main.py
+++ Project 0/main.py
@@ -11,9 +11,13 @@ df = pd.read_excel('inventory.xlsx')
 products_per_supplier = df.groupby('Supplier')['Product Number'].count().to_dict()
 print(products_per_supplier)
 
-# IN PROGRESS - Calculate total value of inventory per supplier
-# total_value_per_supplier = df.groupby('Supplier')[['Inventory', 'Price']].agg({'Inventory': 'sum', 'Price': 'mean'}).eval('Inventory * Price').to_dict()
-# print(total_value_per_supplier)
+# Purpose - Calculate total value of inventory per supplier
+df['Inventory'] = pd.to_numeric(df['Inventory'], errors='coerce')
+df['Inventory'] = df['Inventory'].astype(float)
+df['Price'] = pd.to_numeric(df['Price'], errors='coerce')
+df['Price'] = df['Price'].astype(float)
+total_value_per_supplier = df.groupby('Supplier')[['Inventory', 'Price']].agg({'Inventory': 'sum', 'Price': 'mean'}).eval('Inventory * Price').round(2).to_dict()
+print(total_value_per_supplier)
 
 # IN PROGRESS - Find products with inventory less than 10
 # products_under_10_inv = df[df['Inventory'] < 10]['Product Number'].to_dict()
